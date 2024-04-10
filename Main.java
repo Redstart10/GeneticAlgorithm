79ab253048ab2d913ec6fccc8ec04877a36c5674
Redstart10 <96241945+Redstart10@users.noreply.github.com>
2024-04-10 11:08:56 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Window window = new Window();
+        javax.swing.SwingUtilities.invokeLater(window);
+    }
+}
